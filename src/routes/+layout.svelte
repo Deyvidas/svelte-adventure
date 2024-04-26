a444422237c839a75c013f61f726b5e8753036b5
Deyvidas <devidbanyavichyus@yandex.ru>
2024-04-26 10:18:21 +0400

    initial

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<header>
+    <span>[header]</span>
+    <a href="/">homepage</a>
+    <a href="/about">about</a>
+    <a href="/settings">settings</a>
+</header>
+
+<slot></slot>
+
+<footer>
+    <span>[footer]</span>
+    <a href="/">homepage</a>
+    <a href="/about">about</a>
+    <a href="/settings">settings</a>
+</footer>
